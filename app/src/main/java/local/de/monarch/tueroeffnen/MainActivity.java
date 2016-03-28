52d4316897164e064fd0746e04cb8e3a7db82f82
niels <niels@monarch.de>
2016-03-28 17:29:13 +0200

    Urcode

--- app/src/main/java/local/de/monarch/tueroeffnen/MainActivity.java
+++ app/src/main/java/local/de/monarch/tueroeffnen/MainActivity.java
@@ -1,13 +1,88 @@
 package local.de.monarch.tueroeffnen;
 
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    TextView code;
+    String deviceId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnClick = (Button) findViewById(R.id.button) ;
+        btnClick.setOnClickListener(this);
+        this.code = (TextView) findViewById(R.id.textBoxCode);
+        this.deviceId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String code= this.code.getText().toString();
+        String md5Code = this.md5(code);
+        String md5devId = this.md5(this.deviceId);
+        HttpClient httpclient = new DefaultHttpClient();
+        try {
+            HttpResponse response = httpclient.execute(new HttpGet("http://www.monarch.de?code=" + md5Code + "&devId="+md5devId));
+            StatusLine statusLine = response.getStatusLine();
+            if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                response.getEntity().writeTo(out);
+                String responseString = out.toString();
+                out.close();
+                //..more logic
+            } else {
+                //Closes the connection.
+                response.getEntity().getContent().close();
+                throw new IOException(statusLine.getReasonPhrase());
+            }
+        }
+        catch (Exception ex)
+        {
+            TextView status = (TextView)findViewById(R.id.textViewStatus);
+            status.setText("Fehler: "+ ex.getMessage());
+        }
+    }
+
+    private String md5(final String s) {
+        final String MD5 = "MD5";
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest.getInstance(MD5);
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuilder hexString = new StringBuilder();
+            for (byte aMessageDigest : messageDigest) {
+                String h = Integer.toHexString(0xFF & aMessageDigest);
+                while (h.length() < 2)
+                    h = "0" + h;
+                hexString.append(h);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
