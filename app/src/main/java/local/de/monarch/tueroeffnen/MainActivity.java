469140b4c8af291df39dc78a3d97320230e5c611
niels <niels@MONARCH.DE.LOCAL>
2016-03-28 16:19:43 +0200

    Initial commit

---
+++ app/src/main/java/local/de/monarch/tueroeffnen/MainActivity.java
@@ -0,0 +1,13 @@
+package local.de.monarch.tueroeffnen;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
